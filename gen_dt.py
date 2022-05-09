2f4ab39cbdef437a94560bf8f6f2e2a9b944582e
Ziyue Wu <670122025@qq.com>
2022-05-09 22:06:29 +0800

    first version

---
+++ gen_dt.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+
+'''
+
+T根据X生成，Y根据T和X生成
+X包括gender(X1), cluster(X2, dummies), age(X3)
+
+T0=5*X1+5*(X2=1)+0.1*X3
+T1=5*X1+5*(X2=2)+0.1*X3
+T2=5*X1+5*(X2=3)+0.1*X3
+T3=-5*X1+5*(X2=1)+0.1*X3
+T4=-5*X1+5*(X2=2)+0.1*X3
+T5=-5*X1+5*(X2=3)+0.1*X3
+T6=5*X1+0.1*X3
+T7=-5*X1+0.005*X3*X3
+T0~T7按列normalize到[0,1]
+
+Y=1*X1+3*(X2=1)+1.5*(X2=2)+0.25*(X2=3)-0.01*X3
+    +alpha*T
+
+'''
+
+def mx(vec):
+    Min = min(vec)
+    Max = max(vec)
+    return (vec-min(vec))/(max(vec)-min(vec)+1e-3) + 1e-6
+
+def generate(df):
+    x1 = df['gender']
+    x2_0 = df['cluster_0']
+    x2_1 = df['cluster_1']
+    x2_2 = df['cluster_2']
+    x3 = df['age']
+
+    t0, t1, t2, t3, t4, t5, t6, t7, y = [], [], [], [], [], [], [], [], []
+    for i in range(len(df)):
+        t0.append(5 * x1[i] + 5 * x2_0[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t1.append(5 * x1[i] + 5 * x2_1[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t2.append(5 * x1[i] + 5 * x2_2[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t3.append(-5 * x1[i] + 5 * x2_0[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t4.append(-5 * x1[i] + 5 * x2_1[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t5.append(-5 * x1[i] + 5 * x2_2[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t6.append(5 * x1[i] + 0.1 * x3[i] + np.random.normal(0,1))
+        t7.append(-5 * x1[i] + 0.005 * x3[i] * x3[i] + np.random.normal(0,1))
+
+    # normalize and avoid exact 0 or 1
+    t0, t1, t2, t3, t4, t5, t6, t7 = mx(t0), mx(t1), mx(t2), mx(t3), mx(t4), mx(t5), mx(t6), mx(t7)
+    print(min(t0), min(t1), min(t2), min(t3), min(t4), min(t5), min(t6), min(t7))
+    print(max(t0),max(t1),max(t2),max(t3),max(t4),max(t5),max(t6),max(t7))
+
+    for i in range(len(df)):
+        y.append(1*x1[i]+3*x2_0[i]+1.5*x2_1[i]+0.25*x2_2[i]-0.01*x3[i]+0.2*t0[i]+0.15*t1[i]+0.25*t2[i]+0.44*t3[i]+0.13*t4[i]+0.03*t5[i]-0.14*t6[i]-0.04*t7[i]+ np.random.normal(0,0.5))
+
+    out = pd.DataFrame({
+        't0': t0,
+        't1': t1,
+        't2': t2,
+        't3': t3,
+        't4': t4,
+        't5': t5,
+        't6': t6,
+        't7': t7,
+        'y': y
+    })
+
+    df_out = pd.concat([df, out], axis=1)
+    return df_out
+
+train = pd.read_csv('data/profile_train.csv')
+eval = pd.read_csv('data/profile_eval.csv')
+generate(train).to_csv('data/gen_train.csv',index=False)
+generate(eval).to_csv('data/gen_eval.csv',index=False)
--- mtvae.py
+++ mtvae.py
@@ -32,36 +32,25 @@ class Model(PyroModule):
         ``hidden_dim``, ``num_layers``, and ``outcome_dist``.
     """
 
-    def __init__(self, config):
-        self.latent_dim = config["latent_dim"]
-        self.treat_dim = config['treat_dim']
+    def __init__(self):
+        self.latent_dim = PARAM["latent_dim"]
+        self.hidden_dim = PARAM['hidden_dim']
+        self.treat_dim = PARAM['treat_dim']
+        self.num_layers = PARAM['num_layers']
+        self.xcat_dim = PARAM['xcon_dim']
         super().__init__()
         # f2
-        self.t_nn = MultivariateBetaNet(
-            [config["latent_dim"]]
-            + [config["hidden_dim"]] * config["num_layers"], self.treat_dim
-        )
+        self.t_nn = MultivariateBetaNet([self.latent_dim] + [self.hidden_dim]*self.num_layers,
+                                        self.treat_dim)
         # f3
-        self.y_nn = NormalMeanNet(
-            [config["latent_dim"] + config['treat_dim']]
-            + [config["hidden_dim"]] * config["num_layers"]
-        )
+        self.y_nn = NormalMeanNet([self.latent_dim+self.treat_dim] + [self.hidden_dim]*self.num_layers)
         # f4
-        self.xbi_nn = BernoulliNet(
-            [config["latent_dim"]]
-            + [config["hidden_dim"]] * config["num_layers"]
-        )
+        self.xbi_nn = BernoulliNet([self.latent_dim] + [self.hidden_dim]*self.num_layers)
         # f5
-        self.xcat_nn = CategoricalNet(
-            [config["latent_dim"]]
-            + [config["hidden_dim"]] * config["num_layers"], 3
-        ) # TODO: Num of classes in categorical variables
+        self.xcat_nn = CategoricalNet([self.latent_dim] + [self.hidden_dim]*self.num_layers,
+                                      3) # TODO: Num of classes in categorical variables
         # f6/f7
-        self.xcon_nn = NormalNet(
-            [config["latent_dim"]]
-            + [config["hidden_dim"]] * config["num_layers"]
-            + [config["xcon_dim"]]
-        )
+        self.xcon_nn = NormalNet([self.latent_dim] + [self.hidden_dim]*self.num_layers + [self.xcat_dim])
 
     def forward(self, xbi, xcat, xcon, t=None, y=None, size=None):
         if size is None:
@@ -124,24 +113,20 @@ class Guide(PyroModule):
         ``hidden_dim``, ``num_layers``, and ``outcome_dist``.
     """
 
-    def __init__(self, config):
-        self.latent_dim = config["latent_dim"]
-        self.treat_dim = config['treat_dim']
+    def __init__(self):
+        self.latent_dim = PARAM['latent_dim']
+        self.hidden_dim = PARAM['hidden_dim']
+        self.treat_dim = PARAM['treat_dim']
+        self.feature_dim = PARAM['xbi_dim']+PARAM['xcat_dim']+PARAM['xcon_dim']
         super().__init__()
-        # TODO: check whether g3 suitable
-        self.t_nn = MultivariateBetaNet(
-            [config["feature_dim"]], self.treat_dim
-        )
+        self.t_nn = MultivariateBetaNet([self.feature_dim], self.treat_dim)
         # g4
-        self.y_nn = NormalMeanNet(
-            [config["feature_dim"]+config["treat_dim"], config['hidden_dim']]
-        )
+        self.y_nn = NormalMeanNet([self.feature_dim + self.treat_dim,
+                                   self.hidden_dim])
         # g5
-        self.z_nn = DiagNormalNet(
-            [config["feature_dim"]+config["treat_dim"]+1,
-             config["hidden_dim"],
-            config["latent_dim"]]
-        )
+        self.z_nn = DiagNormalNet([self.feature_dim + self.treat_dim + 1,
+            self.hidden_dim,
+            self.latent_dim])
 
     def forward(self, x_bi, x_cat, x_con, t=None, y=None, size=None):
         if x_bi.dim()==1:
@@ -171,7 +156,7 @@ class Guide(PyroModule):
 
     # q(t|x)
     def t_dist(self, x):
-        Alpha, Beta = self.t_nn(x)
+        Alpha, Beta = self.t_nn(x)  # TODO: beta distribution causes t value nears 1 or 0 infeasible
         return dist.Beta(Alpha, Beta).to_event(1)
     # q(y|t,x)
     def y_dist(self, t, x):
@@ -180,57 +165,58 @@ class Guide(PyroModule):
         return self.y_nn.make_dist(mu_yq)
     # q(z|x,t,y)
     def z_dist(self, y, t, x):
-        ytx = torch.cat((y.unsqueeze(1),t,x), dim=1).float()  # concat
+        ytx = torch.cat((y.unsqueeze(1),t,x), dim=1).float()  # concat, ytx: (batch_size, feature_dim + treatment_dim + 1)
         mu, sigma = self.z_nn(ytx)
         return dist.Normal(mu, sigma).to_event(1)
 
-class MTVAE(nn.Module):
-    def __init__(self, xbi_dim, xcat_dim, xcon_dim, treat_dim,
-                 outcome_dist="bernoulli", latent_dim=20, hidden_dim=200, num_layers=3, num_samples=100):
-        super().__init__()
-        config = dict(
-            # dimension of 'x'
-            xbi_dim=xbi_dim,
-            xcat_dim=xcat_dim,
-            xcon_dim=xcon_dim,
-            feature_dim=xbi_dim+xcat_dim+xcon_dim,
-            treat_dim=treat_dim,        # dimension of treatment 't'
-            latent_dim=latent_dim,      # dimension of latent variable 'z'
-            hidden_dim=hidden_dim,      # dimension of hidden layers of FC
-            num_layers=num_layers,      # numbers of hidden layers in FCN
-            num_samples=num_samples,
+class TraceCausalEffect_ELBO(Trace_ELBO):
+    def _differentiable_loss_particle(self, model_trace, guide_trace):
+        # Construct -ELBO part.
+        blocked_names = [
+            name
+            for name, site in guide_trace.nodes.items()
+            if site["type"] == "sample" and site["is_observed"]
+        ]
+        blocked_guide_trace = guide_trace.copy()
+        for name in blocked_names:
+            del blocked_guide_trace.nodes[name]
+        loss, surrogate_loss = super()._differentiable_loss_particle(
+            model_trace, blocked_guide_trace
         )
-        for name, size in config.items():
-            if not (isinstance(size, int) and size > 0):
-                raise ValueError("Expected {} > 0 but got {}".format(name, size))
-        config["outcome_dist"] = outcome_dist
-        self.xbi_dim = xbi_dim
-        self.xcat_dim = xcat_dim
-        self.xcon_dim = xcon_dim
-        self.treat_dim = treat_dim
-        self.num_samples = num_samples
+        return loss, surrogate_loss
 
-        self.model = Model(config)
-        self.guide = Guide(config)
+    @torch.no_grad()
+    def loss(self, model, guide, *args, **kwargs):
+        return torch_item(self.differentiable_loss(model, guide, *args, **kwargs))
 
-    def fit(self, x_bi, x_cat, x_con, t, y,
-            num_epochs=100, batch_size=100, learning_rate=1e-3, learning_rate_decay=0.1, weight_decay=1e-4, log_every=100):
-        '''
-        :param num_epochs: number of training epochs
-        :param log_every: log loss each this-many steps.
-        :return: list of epoch losses
-        '''
+class MTVAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.xbi_dim = PARAM['xbi_dim']
+        self.xcat_dim = PARAM['xcat_dim']
+        self.xcon_dim = PARAM['xcon_dim']
+        self.treat_dim = PARAM['treat_dim']
+        self.mc_samples = PARAM['mc_samples']
+
+        self.model = Model()
+        self.guide = Guide()
+
+    def fit(self, x_bi, x_cat, x_con, t, y):
+        num_epochs, batch_size, learning_rate, learning_rate_decay, weight_decay, log_every = \
+            PARAM['num_epochs'], PARAM['batch_size'], PARAM['learning_rate'], PARAM['learning_rate_decay'], PARAM['weight_decay'], PARAM['log_every']
         assert x_bi.dim() == 2 and x_bi.size(-1) == self.xbi_dim
         assert x_cat.dim() == 2 and x_cat.size(-1) == self.xcat_dim
         assert x_con.dim() == 2 and x_con.size(-1) == self.xcon_dim
         assert t.size(-1) == self.treat_dim
+
         dataset = TensorDataset(x_bi, x_cat, x_con, t, y)
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         print("Training with {} minibatches per epoch".format(len(dataloader)))
         num_steps = num_epochs * len(dataloader)
         optim = ClippedAdam({"lr": learning_rate,"weight_decay": weight_decay,"lrd": learning_rate_decay ** (1 / num_steps),})
-        # training using :class: '~pyro.infer.svi.SVI' with the 'Trace_ELBO' loss
-        svi = SVI(self.model, self.guide, optim, Trace_ELBO())  # TODO: prepare the ELBO estimator
+        # training using :class : '~pyro.infer.svi.SVI' with the 'Trace_ELBO' loss
+        svi = SVI(self.model, self.guide, optim, TraceCausalEffect_ELBO())  # TODO: prepare the ELBO estimator
         losses = []
         for epoch in range(num_epochs):
             for x_bi, x_cat, x_con, t, y in dataloader:
@@ -242,38 +228,33 @@ class MTVAE(nn.Module):
         return losses
 
     @torch.no_grad()
-    def potential_y(self, x_bi, x_cat, x_con, t, num_samples=None, batch_size=None):
-        '''
-        computes the potential outcome 'y' for a batch of data 'x'
-        :param num_samples: the number of monte carlo samples
-        :return: a 'len(x)' sized tensor of estimated values
-        '''
-        if num_samples is None:
-            num_samples = self.num_samples
+    def potential_y(self, x_bi, x_cat, x_con, t):
+        mc_samples = self.mc_samples
         if not torch._C._get_tracing_state():
             assert x_bi.dim() == 2 and x_bi.size(-1) == self.xbi_dim
             assert x_cat.dim() == 2 and x_cat.size(-1) == self.xcat_dim
             assert x_con.dim() == 2 and x_con.size(-1) == self.xcon_dim
 
         # x_bi: (num_sample, xbi_dim), t: (num_sample, treat_dim)
-        d=torch.cat((x_bi, x_cat, x_con, t),dim=-1) # d: (num_sample, feature_dim+treat_dim)
+        d=torch.cat((x_bi, x_cat, x_con, t),dim=-1) # d: (len(data), feature_dim+treat_dim)
         dataloader = [d]
         print("Evaluating {} minibatches".format(len(dataloader)))
         result = []
         hide_list = ['t'+str(i) for i in range(self.treat_dim)]+['y']
         for d in dataloader:
-            # d: (num_sample, feature_dim+treat_dim)
-            with pyro.plate("num_particles", num_samples, dim=-2):
-                print("num_samples:", num_samples)
+            # d: (len(data), feature_dim+treat_dim)
+            with pyro.plate("num_particles", mc_samples, dim=-2):
                 with poutine.trace() as tr, poutine.block(hide=hide_list):
                     self.guide(d[:,0],d[:,1],d[:,2])    # xbi, xcat, xcon
 
                 with poutine.do(data=dict(t=torch.tensor(d[:,3:]))):
                     y_hat = poutine.replay(self.model.y_mean, tr.trace)(d[:,0],d[:,1],d[:,2])
 
-            if not torch._C._get_tracing_state():
-                print("batch y_pot = {:0.6g}".format(y_hat.mean()))
+            # y_hat: (len(data))
+            # if not torch._C._get_tracing_state():
+            #     print("mean y_hat = {:0.6g}".format(y_hat.mean()))
             result.append(y_hat)    # ite: (len(x))
+        # len(result)=1
         return torch.cat(result)
 
     def to_script_module(self):
@@ -313,56 +294,47 @@ def main():
     # Generate synthetic data.
     pyro.set_rng_seed(PARAM['seed'])
 
-    x_bi, x_cat, x_con, t, y = get_dt('data/network_train.csv')
+    x_bi, x_cat, x_con, t, y = get_dt('data/gen_train.csv')
 
     # Train.
     pyro.set_rng_seed(PARAM['seed'])
     pyro.clear_param_store()
-    mtvae = MTVAE(
-        xbi_dim=PARAM['xbi_dim'],
-        xcat_dim=PARAM['xcat_dim'],
-        xcon_dim=PARAM['xcon_dim'],
-        treat_dim=PARAM['treat_dim'],
-        latent_dim=PARAM['latent_dim'],
-        hidden_dim=PARAM['hidden_dim'],
-        num_layers=PARAM['num_layers'],
-        num_samples=10,
-    )
-    mtvae.fit(x_bi, x_cat, x_con, t, y,
-        num_epochs=PARAM['num_epochs'],
-        batch_size=PARAM['batch_size'],
-        learning_rate=PARAM['learning_rate'],
-        learning_rate_decay=PARAM['learning_rate_decay'],
-        weight_decay=PARAM['weight_decay'],
-    )
+    mtvae = MTVAE()
+    mtvae.fit(x_bi, x_cat, x_con, t, y)
 
     # Evaluate.
-    x_bi, x_cat, x_con, t, y = get_dt('data/network_eval.csv')
+    x_bi, x_cat, x_con, t, y = get_dt('data/gen_eval.csv')
+    est_y = mtvae.potential_y(x_bi, x_cat, x_con, t)    # est_y: (len(data))
 
     # estimate the potential outcome
-    true_y_mean = y.mean()
-    print("true outcome = {:0.3g}".format(true_y_mean.item()))
-    est_y = mtvae.potential_y(x_bi, x_cat, x_con, t)
+    true_ave_y = y.mean()
     est_ave_y = est_y.mean()
+    print("true outcome = {:0.3g}".format(true_ave_y.item()))
     print("estimated Average potential outcome = {:0.3g}".format(est_ave_y.item()))
 
 PARAM = {
     'description': "Multiple Treatment Causal Effect Variational Autoencoder",
+    # data
     'num_data': 100,
     'xbi_dim': 1,
     'xcat_dim': 1,
     'xcon_dim': 1,
     'treat_dim': 8,
-    'latent_dim': 20,
+    # model
+    'latent_dim': 25,
     'hidden_dim': 200,
     'num_layers': 3,
-    'num_epochs': 20,
+    # train
+    'num_epochs': 100,
     'batch_size': 20,
     'learning_rate': 1e-3,
     'learning_rate_decay': 0.1,
-    'weight_decay': 0.01,
+    'weight_decay': 1e-4,
+    'log_every': 100,
     'seed': 100,
     'cuda': False,
+    # eval
+    'mc_samples': 50,  # number of monte carlo sample
 }
 
 if __name__ == "__main__":
